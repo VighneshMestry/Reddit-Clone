f66e234b70ac83b459081f0aa4cdf2195c6d116c
vighnesh <mestryvighnesh27@gmail.com>
2024-01-17 20:16:12 +0530

    add posts

---
+++ lib/features/post/controller/post_controller.dart
@@ -0,0 +1,149 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reddit_clone/core/providers/storage_repository_provider.dart';
+import 'package:reddit_clone/core/utils.dart';
+import 'package:reddit_clone/features/auth/controller/auth_controller.dart';
+import 'package:reddit_clone/features/post/repository/post_repository.dart';
+import 'package:reddit_clone/models/community_model.dart';
+import 'package:reddit_clone/models/post_model.dart';
+import 'package:routemaster/routemaster.dart';
+import 'package:uuid/uuid.dart';
+
+final postControllerProvider = StateNotifierProvider<PostController, bool>((ref) {
+  return PostController(
+    storageRepository: ref.read(storageRepositoryProvider),
+    ref: ref,
+    postRepository: ref.read(postRepositoryProvider),
+  );
+});
+
+class PostController extends StateNotifier<bool> {
+  StorageRepository _storageRepository;
+  Ref _ref;
+  PostRepository _postRepository;
+  PostController({
+    required StorageRepository storageRepository,
+    required Ref ref,
+    required PostRepository postRepository,
+  })  : _storageRepository = storageRepository,
+        _ref = ref,
+        _postRepository = postRepository,
+        super(false);
+
+  void addTextPost({
+    required BuildContext context,
+    required String title,
+    required Community selectedCommunity,
+    required String description,
+  }) async {
+    state = true;
+    final postId = Uuid().v1();
+    final user = _ref.read(userProvider)!;
+
+    Post post = Post(
+      id: postId,
+      title: title,
+      communityName: selectedCommunity.name,
+      communityProfilePic: selectedCommunity.avatar,
+      upvotes: [],
+      downvotes: [],
+      commentCount: 0,
+      username: user.name,
+      uid: user.uid,
+      type: "text",
+      createdAt: DateTime.now(),
+      awards: [],
+      description: description,
+    );
+
+    final res = await _postRepository.addPost(post);
+    state = false;
+    res.fold(
+      (l) => showSnackBar(context, l.message),
+      (r) {
+        showSnackBar(context, "Posted successfully!");
+        Routemaster.of(context).pop();
+      },
+    );
+  }
+
+  void addLinkPost({
+    required BuildContext context,
+    required String title,
+    required Community selectedCommunity,
+    required String link,
+  }) async {
+    state = true;
+    final postId = Uuid().v1();
+    final user = _ref.read(userProvider)!;
+
+    Post post = Post(
+      id: postId,
+      title: title,
+      communityName: selectedCommunity.name,
+      communityProfilePic: selectedCommunity.avatar,
+      upvotes: [],
+      downvotes: [],
+      commentCount: 0,
+      username: user.name,
+      uid: user.uid,
+      type: "link",
+      createdAt: DateTime.now(),
+      awards: [],
+      description: link,
+    );
+
+    final res = await _postRepository.addPost(post);
+    state = false;
+    res.fold(
+      (l) => showSnackBar(context, l.message),
+      (r) {
+        showSnackBar(context, "Posted successfully!");
+        Routemaster.of(context).pop();
+      },
+    );
+  }
+
+  void addImagePost({
+    required BuildContext context,
+    required String title,
+    required Community selectedCommunity,
+    required File file,
+  }) async {
+    state = true;
+    final postId = Uuid().v1();
+    final user = _ref.read(userProvider)!;
+    final imageRes = await _storageRepository.storeFile(
+        path: "posts/${selectedCommunity.name}", id: postId, file: file);
+
+    imageRes.fold((l) => showSnackBar(context, l.message), (r) async {
+      Post post = Post(
+        id: postId,
+        title: title,
+        communityName: selectedCommunity.name,
+        communityProfilePic: selectedCommunity.avatar,
+        upvotes: [],
+        downvotes: [],
+        commentCount: 0,
+        username: user.name,
+        uid: user.uid,
+        type: "image",
+        createdAt: DateTime.now(),
+        awards: [],
+        link: r,
+      );
+
+      final res = await _postRepository.addPost(post);
+      state = false;
+      res.fold(
+        (l) => showSnackBar(context, l.message),
+        (r) {
+          showSnackBar(context, "Posted successfully!");
+          Routemaster.of(context).pop();
+        },
+      );
+    });
+  }
+}
--- lib/features/post/screens/add_post_type_screen.dart
+++ lib/features/post/screens/add_post_type_screen.dart
@@ -7,6 +7,7 @@ import 'package:reddit_clone/core/common/error_line.dart';
 import 'package:reddit_clone/core/common/loader.dart';
 import 'package:reddit_clone/core/utils.dart';
 import 'package:reddit_clone/features/community/controller/community_controller.dart';
+import 'package:reddit_clone/features/post/controller/post_controller.dart';
 import 'package:reddit_clone/models/community_model.dart';
 import 'package:reddit_clone/theme/pallete.dart';
 
@@ -25,6 +26,7 @@ class AddPostTypeScreen extends ConsumerStatefulWidget {
 class _AddPostTypeScreenState extends ConsumerState<AddPostTypeScreen> {
   final titleController = TextEditingController();
   final descriptionController = TextEditingController();
+  final linkController = TextEditingController();
   List<Community> communities = [];
   Community? selectedCommunity;
   @override
@@ -46,101 +48,141 @@ class _AddPostTypeScreenState extends ConsumerState<AddPostTypeScreen> {
     }
   }
 
+  void share() {
+    if (widget.type == "image" &&
+        bannerFile != null &&
+        titleController.text.isNotEmpty) {
+      ref.read(postControllerProvider.notifier).addImagePost(
+            context: context,
+            title: titleController.text.trim(),
+            selectedCommunity: selectedCommunity ?? communities[0],
+            file: bannerFile!,
+          );
+    } else if (widget.type == "text" && titleController.text.isNotEmpty) {
+      ref.read(postControllerProvider.notifier).addTextPost(
+            context: context,
+            title: titleController.text.trim(),
+            selectedCommunity: selectedCommunity ?? communities[0],
+            description: descriptionController.text.trim(),
+          );
+    } else if (widget.type == "link" && linkController.text.isNotEmpty && 
+        titleController.text.isNotEmpty) {
+      ref.read(postControllerProvider.notifier).addLinkPost(
+            context: context,
+            title: titleController.text.trim(),
+            selectedCommunity: selectedCommunity ?? communities[0],
+            link: linkController.text.trim(),
+          );
+    } else {
+      showSnackBar(context, "Please enter all fields");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final currentTheme = ref.watch(themeNotifierProvider);
     final isTypeImage = widget.type == 'image';
     final isTypeText = widget.type == 'text';
     final isTypeLink = widget.type == 'link';
+    final isLoading = ref.watch(postControllerProvider);
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: Text("Add ${widget.type}"),
-        actions: [TextButton(onPressed: () {}, child: Text("share"))],
+        actions: [
+          TextButton(onPressed: () => share(), child: Text("share")),
+        ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: titleController,
-              maxLength: 30,
-              decoration: const InputDecoration(
-                filled: true,
-                hintText: 'Enter title here',
-                border: InputBorder.none,
-                contentPadding: EdgeInsets.all(18),
+      body: isLoading
+          ? const Loader()
+          : Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  TextField(
+                    controller: titleController,
+                    maxLength: 30,
+                    decoration: const InputDecoration(
+                      filled: true,
+                      hintText: 'Enter title here',
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.all(18),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  if (isTypeImage)
+                    GestureDetector(
+                      onTap: selectBannerImage,
+                      child: DottedBorder(
+                        borderType: BorderType.RRect,
+                        radius: const Radius.circular(10),
+                        dashPattern: const [10, 4],
+                        strokeCap: StrokeCap.round,
+                        color: currentTheme.textTheme.bodyText2!.color!,
+                        child: Container(
+                          width: double.infinity,
+                          height: 150,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: bannerFile != null
+                              ? Image.file(bannerFile!)
+                              : const Center(
+                                  child: Icon(
+                                    Icons.camera_alt_outlined,
+                                    size: 40,
+                                  ),
+                                ),
+                        ),
+                      ),
+                    ),
+                  if (isTypeText)
+                    TextField(
+                      controller: descriptionController,
+                      maxLines: 5,
+                      decoration: const InputDecoration(
+                        filled: true,
+                        hintText: 'Enter description',
+                        border: InputBorder.none,
+                        contentPadding: EdgeInsets.all(18),
+                      ),
+                    ),
+                  if (isTypeLink)
+                    TextField(
+                      controller: linkController,
+                      decoration: const InputDecoration(
+                        filled: true,
+                        hintText: 'Enter link',
+                        border: InputBorder.none,
+                        contentPadding: EdgeInsets.all(18),
+                      ),
+                    ),
+                  const SizedBox(height: 20),
+                  const Align(
+                      alignment: Alignment.topLeft,
+                      child: Text("Select Community")),
+                  ref.watch(getUserCommunitiesProvider).when(
+                      data: (data) {
+                        communities = data;
+                        return DropdownButton(
+                          value: selectedCommunity ?? data[0],
+                          items: data
+                              .map((e) => DropdownMenuItem(
+                                  value: e, child: Text(e.name)))
+                              .toList(),
+                          onChanged: (val) {
+                            setState(() {
+                              selectedCommunity = val;
+                            });
+                          },
+                        );
+                      },
+                      error: (error, stackTrace) =>
+                          ErrorLine(error: error.toString()),
+                      loading: () => const Loader())
+                ],
               ),
             ),
-            const SizedBox(height: 10),
-            if (isTypeImage)
-              DottedBorder(
-                borderType: BorderType.RRect,
-                radius: const Radius.circular(10),
-                dashPattern: const [10, 4],
-                strokeCap: StrokeCap.round,
-                color: currentTheme.textTheme.bodyText2!.color!,
-                child: Container(
-                  width: double.infinity,
-                  height: 150,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: bannerFile != null
-                      ? Image.file(bannerFile!)
-                      : const Center(
-                          child: Icon(
-                            Icons.camera_alt_outlined,
-                            size: 40,
-                          ),
-                        ),
-                ),
-              ),
-            if (isTypeText)
-              TextField(
-                controller: descriptionController,
-                maxLines: 5,
-                decoration: const InputDecoration(
-                  filled: true,
-                  hintText: 'Enter description',
-                  border: InputBorder.none,
-                  contentPadding: EdgeInsets.all(18),
-                ),
-              ),
-            if (isTypeLink)
-              TextField(
-                controller: descriptionController,
-                decoration: const InputDecoration(
-                  filled: true,
-                  hintText: 'Enter link',
-                  border: InputBorder.none,
-                  contentPadding: EdgeInsets.all(18),
-                ),
-              ),
-            const SizedBox(height: 20),
-            const Align(
-                alignment: Alignment.topLeft, child: Text("Select Community")),
-            ref.watch(getUserCommunitiesProvider).when(
-                data: (data) {
-                  communities = data;
-                  return DropdownButton(
-                    value: selectedCommunity ?? data[0],
-                    items: data
-                        .map((e) => DropdownMenuItem(value: e, child: Text(e.name)))
-                        .toList(),
-                    onChanged: (val) {
-                      setState(() {
-                        selectedCommunity = val;
-                      });
-                    },
-                  );
-                },
-                error: (error, stackTrace) =>
-                    ErrorLine(error: error.toString()),
-                loading: () => const Loader())
-          ],
-        ),
-      ),
     );
   }
 }
